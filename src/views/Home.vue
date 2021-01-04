77e5b72ae0866da654bac20dda015ae102c86aaf
HanRui <mail@hr.im>
2021-01-04 15:00:05 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
+<!--
+ * @Author: HanRui(JoyNop)
+ * @Date: 2021-01-04 14:41:01
+ * @LastEditors: HanRui(JoyNop)
+ * @LastEditTime: 2021-01-04 14:54:16
+ * @Description: file content
+ * @FilePath: /electron-vue-demo/src/views/Home.vue
+-->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div>By JoyNop</div>
+
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
